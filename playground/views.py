90359622f9c83510381367f21a82644af7908624
HtetMyoAung <htetmyoaung37@gmail.com>
2026-05-28 01:03:19 +0900

    selecting fields to QuerySet and render them in the template

--- playground/views.py
+++ playground/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.db.models import Q, F
-from store.models import Product
+from store.models import Product, OrderItem
 
 
 def say_hello(request):
-    query_set = Product.objects.filter(Q(inventory__lt=10) | Q(
-        unit_price__lt=20)).order_by('unit_price')[5:10]
-    return render(request, 'hello.html', {'name': 'Xiao', 'products': query_set})
+    queryset = Product.objects.filter(
+        id__in=OrderItem.objects.values('product_id').distinct()).order_by('title')
+
+    return render(request, 'hello.html', {'name': 'Xiao', 'products': queryset})
