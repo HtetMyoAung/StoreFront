f1f3f049d87ccef3cc995216fffbb3bf52d0f597
HtetMyoAung <htetmyoaung37@gmail.com>
2026-05-30 14:44:35 +0900

    Setting up the admin site with custom headers and titles for the StoreFront project.

--- storefront/urls.py
+++ storefront/urls.py
@@ -3,6 +3,9 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 
+admin.site.site_header = 'StoreFront Admin'
+admin.site.site_title = 'StoreFront Admin Portal'
+admin.site.index_title = 'Welcome to StoreFront Admin Portal'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
