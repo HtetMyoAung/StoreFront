dd8d73e7c2045a1ab1c62a07a51b6a49e8bf055e
HtetMyoAung <htetmyoaung37@gmail.com>
2026-05-31 03:31:24 +0900

    Providing Link to other

--- store/admin.py
+++ store/admin.py
@@ -1,5 +1,10 @@
+from urllib.parse import urlencode
+
 from django.contrib import admin
+from django.urls import reverse
 from . import models
+from django.db.models import Count
+from django.utils.html import format_html
 
 
 @admin.register(models.Product)
@@ -38,14 +43,15 @@ class CollectionAdmin(admin.ModelAdmin):
 
     @admin.display(ordering='product_count')
     def Collection_count(self, collection):
-        return collection.product_set.count()
-
-    def get_queryset(self, request):
-        return super().get_queryset(request).annotate(product_count=models.Count('product'))
+        # Generate a link to the admin page that lists products filtered by this collection
+        url = (reverse('admin:store_product_changelist') + '?' +
+               urlencode({'collection__id': str(collection.id)}))
+    # Return the HTML for the link, displaying the product count
+        return format_html('<a href="{}">{}</a>', url, collection.product_set.count())
 
 
 @admin.register(models.Order)
-class OrderAmin(admin.ModelAdmin):
+class OrderAdmin(admin.ModelAdmin):
     list_display = ['id', 'placed_at', 'payment_status', 'customer']
     # Query optimization
     list_select_related = ['customer']
