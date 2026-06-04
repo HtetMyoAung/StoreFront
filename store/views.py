08fd43c7bb917479d940e5de554f71d097045703
HtetMyoAung <htetmyoaung37@gmail.com>
2026-06-04 23:33:35 +0900

    Serializing the product model using Django REST Framework serializers

--- store/views.py
+++ store/views.py
@@ -1,15 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .serializers import ProductSerializer
+from .models import Product
 
 
 @api_view()
 def product_list(request):
-    return Response('ok')
+    # get all products from the database, serialize them, and return them in the response
+    queryset = Product.objects.all()
+    serializer = ProductSerializer(queryset, many=True)
+    return Response(serializer.data)
 
 
 @api_view()
 # created a view function for product details, which takes an id parameter
 def product_detail(request, id):
-    return Response('id')
+    # get the product with the given id, and return a 404 error if it doesn't exist
+    product = get_object_or_404(Product, pk=id)
+    # serialize the product and return it in the response
+    serializer = ProductSerializer(product)
+    return Response(serializer.data)
