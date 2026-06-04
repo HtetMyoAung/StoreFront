419c5cfcfd22c4d5f8205469e99522803480c6eb
HtetMyoAung <htetmyoaung37@gmail.com>
2026-06-05 00:11:24 +0900

    create custom serializer for product model

--- store/serializers.py
+++ store/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
+from decimal import Decimal
 
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=200)
-    unit_price = serializers.DecimalField(max_digits=10, decimal_places=2)
+    price = serializers.DecimalField(
+        max_digits=10, decimal_places=2, source='unit_price')
+    price_with_tax = serializers.SerializerMethodField(
+        method_name='get_price_with_tax')
+
+    def get_price_with_tax(self, product):
+        return product.unit_price * Decimal('1.1')
