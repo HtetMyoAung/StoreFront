4ff8a9a34a17acad61c8fb63737a638306f9357d
HtetMyoAung <htetmyoaung37@gmail.com>
2026-06-06 14:51:41 +0900

    Implement CRUD operations for collection endpoint

--- store/urls.py
+++ store/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path('products', views.product_list, name='product_list'),
     # created a path for product details, adding an id parameter to the url, and passing it to the view function
     path('products/<int:id>', views.product_detail, name='product_detail'),
+    # created a path for collection list, and passing it to the view function
+    path('collections', views.collection_list, name='collection_list'),
+    # created a path for collection details, adding a pk parameter to the url, and passing it to the view function
+    path('collections/<int:pk>', views.collection_detail, name='collection_detail'),
 ]
--- store/views.py
+++ store/views.py
@@ -3,8 +3,9 @@ from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from .serializers import ProductSerializer
-from .models import Product
+from .serializers import CollectionSerializer, ProductSerializer
+from .models import Collection, Product
+from django.db.models import Count
 
 
 @api_view(['GET', 'POST'])
@@ -25,7 +26,7 @@ def product_list(request):
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
-@api_view(['GET', 'PUT', 'PATCH', 'DELETE'])
+@api_view(['GET', 'PUT', 'DELETE'])
 # created a view function for product details, which takes an id parameter
 def product_detail(request, id):
     # get the product with the given id, and return a 404 error if it doesn't exist
@@ -42,8 +43,42 @@ def product_detail(request, id):
         # save the updated product to the database
         serializer.save()
         return Response(serializer.data)
+    elif request.method == 'DELETE':
+        if product.orderitems.count() > 0:
+            return Response({"error": "Cannot delete product with existing orders"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-@api_view()
+@api_view(['GET', 'PUT'])
+def collection_list(request):
+    if request.method == 'GET':
+        queryset = Collection.objects.annotate(
+            products_count=Count('products')).all()
+        serializer = CollectionSerializer(queryset, many=True)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = CollectionSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+
+@api_view(['GET', 'PUT', 'DELETE'])
 def collection_detail(request, pk):
-    return HttpResponse('ok')
+    collection = get_object_or_404(Collection.objects.annotate(
+        products_count=Count('products')), pk=pk)
+    if request.method == 'GET':
+        serializer = CollectionSerializer(collection)
+        return Response(serializer.data)
+    elif request.method == 'PUT':
+        serializer = CollectionSerializer(
+            collection, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    elif request.method == 'DELETE':
+        if collection.products.count() > 0:
+            return Response({"error": "Cannot delete collection with existing products"}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        collection.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
