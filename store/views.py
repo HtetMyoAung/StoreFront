0213ebdd9c99c4e95a7a2ac37fe826eff8a1a11b
HtetMyoAung <htetmyoaung37@gmail.com>
2026-06-06 13:26:50 +0900

    adding data validation to the product creation endpoint

--- store/views.py
+++ store/views.py
@@ -2,19 +2,27 @@ from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from rest_framework import status
 from .serializers import ProductSerializer
 from .models import Product
 
 
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request):
-    # get all products from the database, serialize them, and return them in the response
-    queryset = Product.objects.all()
-    serializer = ProductSerializer(queryset, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        # get all products from the database, serialize them, and return them in the response
+        queryset = Product.objects.all()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        # create a new product
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        print(serializer.validated_data)
+        return Response('ok')
 
 
-@api_view()
+@api_view(['GET', 'PUT', 'DELETE'])
 # created a view function for product details, which takes an id parameter
 def product_detail(request, id):
     # get the product with the given id, and return a 404 error if it doesn't exist
