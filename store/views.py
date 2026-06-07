c8e77121691d8e833766fefa85487080fc07577c
HtetMyoAung <htetmyoaung37@gmail.com>
2026-06-07 15:07:35 +0900

    Refactor ProductList and CollectionList view to use shorthand generic attributes

--- store/views.py
+++ store/views.py
@@ -1,32 +1,32 @@
 from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.views import APIView
+from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
 from .serializers import CollectionSerializer, ProductSerializer
 from .models import Collection, Product
 from django.db.models import Count
 
 
-class ProductList(APIView):
-    # get method to retrieve all products and post method to create a new product
-    def get(self, request):
-        # get all products from the database, serialize them, and return them in the response
-        queryset = Product.objects.all()
-        # serialize the queryset and return the serialized data in the response
-        serializer = ProductSerializer(queryset, many=True)
-        return Response(serializer.data)
+class ProductList(ListCreateAPIView):
+    # get method to retrieve all products, and post method to create a new product
+    queryset = Product.objects.select_related('collection').all()
+    serializer_class = ProductSerializer
 
-    def post(self, request):
-        # create a new product
-        serializer = ProductSerializer(data=request.data)
-        # validate the data and raise an exception if it's invalid
-        serializer.is_valid(raise_exception=True)
-        # save the new product to the database
-        serializer.save()
-        print(serializer.validated_data)
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    # get_queryset method to return the queryset of products
+    def get_queryset(self):
+        return Product.objects.select_related('collection').all()
+
+    # serializer_class method to return the serializer class for the products
+    def get_serializer_class(self):
+        return ProductSerializer
+
+    # get_serializer_context method to return the context for the serializer, which includes the request object
+    def get_serializer_context(self):
+        return {'request': self.request}
 
 
 class ProductDetail(APIView):
@@ -56,20 +56,10 @@ class ProductDetail(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class CollectionList(APIView):
-    def get(self, request):
-        # get all collections from the database, annotate them with the count of products in each collection, serialize them, and return them in the response
-        queryset = Collection.objects.annotate(
-            products_count=Count('products')).all()
-        serializer = CollectionSerializer(queryset, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        # create a new collection
-        serializer = CollectionSerializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
-        serializer.save()
-        return Response(serializer.data, status=status.HTTP_201_CREATED)
+class CollectionList(ListCreateAPIView):
+    queryset = Collection.objects.annotate(
+        products_count=Count('products')).all()
+    serializer_class = CollectionSerializer
 
 
 class CollectionDetail(APIView):
