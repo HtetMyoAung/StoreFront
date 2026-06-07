1da4c0979cf5f7f9dbefa1650178448695123b1e
HtetMyoAung <htetmyoaung37@gmail.com>
2026-06-07 15:41:18 +0900

    Refractor the Viewset for Product and Collection

--- store/views.py
+++ store/views.py
@@ -1,37 +1,21 @@
 from django.shortcuts import get_object_or_404
-from django.http import HttpResponse
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
-from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.mixins import ListModelMixin, CreateModelMixin, RetrieveModelMixin, UpdateModelMixin, DestroyModelMixin
+from rest_framework.viewsets import ModelViewSet
 from .serializers import CollectionSerializer, ProductSerializer
 from .models import Collection, Product
 from django.db.models import Count
 
 
-class ProductList(ListCreateAPIView):
-    # get method to retrieve all products, and post method to create a new product
-    queryset = Product.objects.select_related('collection').all()
+class ProductViewSet(ModelViewSet):
+    # viewset automatically provides `list`, `create`, `retrieve`, `update`, and `destroy` actions.
+    queryset = Product.objects.all()
+    #  used for validating and deserializing input, and for serializing output.
     serializer_class = ProductSerializer
 
-    # get_queryset method to return the queryset of products
-    def get_queryset(self):
-        return Product.objects.select_related('collection').all()
-
-    # serializer_class method to return the serializer class for the products
-    def get_serializer_class(self):
-        return ProductSerializer
-
-    # get_serializer_context method to return the context for the serializer, which includes the request object
     def get_serializer_context(self):
         return {'request': self.request}
-
-
-class ProductDetail(RetrieveUpdateDestroyAPIView):
-    queryset = Product.objects.select_related('collection').all()
-    serializer_class = ProductSerializer
 
     def delete(self, request, pk):
         product = get_object_or_404(Product, pk=pk)
@@ -41,13 +25,7 @@ class ProductDetail(RetrieveUpdateDestroyAPIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
-class CollectionList(ListCreateAPIView):
-    queryset = Collection.objects.annotate(
-        products_count=Count('products')).all()
-    serializer_class = CollectionSerializer
-
-
-class CollectionDetail(RetrieveUpdateDestroyAPIView):
+class CollectionViewSet(ModelViewSet):
     queryset = Collection.objects.annotate(
         products_count=Count('products')).all()
     serializer_class = CollectionSerializer
